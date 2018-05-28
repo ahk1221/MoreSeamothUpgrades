1406f0eda3b06a52bd27688a53d8bdbf70f43ac5
ahk1221 <ahmed16.ahk@gmail.com>
2018-05-28 20:32:02 +0500

    Add project files.

---
+++ MoreSeamothUpgrades/Main.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using UnityEngine;
+using SMLHelper;
+using SMLHelper.Patchers;
+using Harmony;
+
+namespace MoreSeamothUpgrades
+{
+    public class Main
+    {
+        public static TechType SeamothHullModule4;
+        public static TechType SeamothHullModule5;
+        public static TechType SeamothDrillModule;
+
+        public static void Patch()
+        {
+            #region Patching
+
+            var harmony = HarmonyInstance.Create("com.ahk1221.moreseamothupgrades");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            LanguagePatcher.customLines.Add("Tooltip_VehicleHullModule3", "Enhances diving depth. Does not stack"); // To update conflicts about the maximity.
+
+            #endregion
+
+            #region Add TechTypes
+
+            SeamothHullModule4 = TechTypePatcher.AddTechType("SeamothHullModule4", "Seamoth depth module MK4", "Enhances diving depth. Does not stack.");
+            SeamothHullModule5 = TechTypePatcher.AddTechType("SeamothHullModule5", "Seamoth depth module MK5", "Enhances diving depth to maximum. Does not stack.");
+            SeamothDrillModule = TechTypePatcher.AddTechType("SeamothDrillModule", "Seamoth drill module", "Allows the Seamoth to drill resource nodes like the PRAWN Drill Arm.");
+
+            #endregion
+
+            #region Equipment Types
+
+            CraftDataPatcher.customEquipmentTypes[SeamothHullModule4] = EquipmentType.SeamothModule;
+            CraftDataPatcher.customEquipmentTypes[SeamothHullModule5] = EquipmentType.SeamothModule;
+            CraftDataPatcher.customEquipmentTypes[SeamothDrillModule] = EquipmentType.SeamothModule;
+
+            #endregion
+
+            #region Add Prefabs
+
+            CustomPrefabHandler.customPrefabs.Add(new CustomPrefab(
+                "SeamothHullModule4",
+                "WorldEntities/Tools/SeamothHullModule4",
+                SeamothHullModule4,
+                GetSeamothHullModuleIV));
+
+            CustomPrefabHandler.customPrefabs.Add(new CustomPrefab(
+                "SeamothHullModule5",
+                "WorldEntities/Tools/SeamothHullModule5",
+                SeamothHullModule5,
+                GetSeamothHullModuleV));
+
+            #endregion
+
+            #region Add Sprites
+            var sprite = SpriteManager.Get(TechType.VehicleHullModule3);
+
+            CustomSpriteHandler.customSprites.Add(new CustomSprite(SeamothHullModule4, sprite));
+            CustomSpriteHandler.customSprites.Add(new CustomSprite(SeamothHullModule5, sprite));
+            
+            #endregion
+        }
+
+        public static GameObject GetSeamothHullModuleIV()
+        {
+            return GetSeamothUpgrade(SeamothHullModule4, "SeamothHullModule4");
+        }
+
+        public static GameObject GetSeamothHullModuleV()
+        {
+            return GetSeamothUpgrade(SeamothHullModule5, "SeamothHullModule5");
+        }
+
+        public static GameObject GetSeamothUpgrade(TechType techType, string classId)
+        {
+            var path = "WorldEntities/Tools/SeamothElectricalDefense";
+            var prefab = Resources.Load<GameObject>(path);
+            var obj = GameObject.Instantiate(prefab);
+
+            var techTag = obj.GetComponent<TechTag>();
+            var prefabIdentifier = obj.GetComponent<PrefabIdentifier>();
+
+            techTag.type = techType;
+            prefabIdentifier.ClassId = classId;
+
+            return obj;
+        }
+    }
+}
---
+++ MoreSeamothUpgrades/Patches/Seamoth_Patches.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Harmony;
+using System.Collections.Generic;
+
+namespace MoreSeamothUpgrades.Patches
+{
+    [HarmonyPatch(typeof(SeaMoth))]
+    [HarmonyPatch("OnUpgradeModuleChange")]
+    public class Seamoth_OnUpgradeModuleChange_Patch
+    {
+        static readonly string[] _slotIDs = new string[]
+        {
+            "SeamothModule1",
+            "SeamothModule2",
+            "SeamothModule3",
+            "SeamothModule4"
+        };
+
+        static void Postfix(SeaMoth __instance, TechType techType)
+        {
+            var dictionary = new Dictionary<TechType, float>
+            {
+                {
+                    TechType.SeamothReinforcementModule,
+                    800f
+                },
+                {
+                    TechType.VehicleHullModule1,
+                    100f
+                },
+                {
+                    TechType.VehicleHullModule2,
+                    300f
+                },
+                {
+                    TechType.VehicleHullModule3,
+                    700f
+                },
+                {
+                    Main.SeamothHullModule4,
+                    1200f
+                },
+                {
+                    Main.SeamothHullModule5,
+                    1500f
+                }
+            };
+
+            var depthUpgrade = 0f;
+            for (int i = 0; i < _slotIDs.Length; i++)
+            {
+                var slot = _slotIDs[i];
+                var techTypeInSlot = __instance.modules.GetTechTypeInSlot(slot);
+
+                if (dictionary.ContainsKey(techTypeInSlot))
+                {
+                    var currentIteration = dictionary[techTypeInSlot];
+                    if (currentIteration > depthUpgrade)
+                    {
+                        depthUpgrade = currentIteration;
+                    }
+                }
+            }
+
+            __instance.crushDamage.SetExtraCrushDepth(depthUpgrade);
+        }
+    }
+
+    [HarmonyPatch(typeof(SeaMoth))]
+    [HarmonyPatch("OnUpgradeModuleUse")]
+}
