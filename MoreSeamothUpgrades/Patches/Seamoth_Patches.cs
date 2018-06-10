14e0e40d922012df4957da5c5d5ab0b802e6c35e
ahk1221 <ahmed16.ahk@gmail.com>
2018-06-10 20:43:44 +0500

    Fixed Thermal damage.

--- MoreSeamothUpgrades/Patches/Seamoth_Patches.cs
+++ MoreSeamothUpgrades/Patches/Seamoth_Patches.cs
@@ -8,6 +8,7 @@ namespace MoreSeamothUpgrades.Patches
 {
     public class SeamothUtility
     {
+        // Copy-paste of SeaMoth._slotIDs.
         public static readonly string[] slotIDs = new string[]
         {
             "SeamothModule1",
@@ -23,16 +24,19 @@ namespace MoreSeamothUpgrades.Patches
     {
         static void Postfix(SeaMoth __instance, int slotID, bool active)
         {
+            // Find the TechType in the toggled slot.
             var techType = __instance.modules.GetTechTypeInSlot(SeamothUtility.slotIDs[slotID]);
-            if(techType == Main.SeamothDrillModule)
-            {
-                ErrorMessage.AddDebug("Found DrillModule");
 
+            // If its the SeamothDrillModule
+            if (techType == Main.SeamothDrillModule)
+            { 
+                // Get the SeamothDrill component from the SeaMoth object.
                 var seamothDrillModule = __instance.GetComponent<SeamothDrill>();
 
+                // If its not null
                 if(seamothDrillModule != null)
                 {
-                    ErrorMessage.AddDebug("Found DrillModule component");
+                    // Set its toggle!
                     seamothDrillModule.toggle = active;
                 }
             }
@@ -45,7 +49,18 @@ namespace MoreSeamothUpgrades.Patches
     {
         static void Prefix(SeaMoth __instance)
         {
+            // Add the SeamothDrill component to the Seamoth on start.
             __instance.gameObject.AddComponent<SeamothDrill>();
+
+            // Get TemperatureDamage class from SeaMoth
+            var tempDamage = __instance.GetComponent<TemperatureDamage>();
+
+            // Set the different fields
+            // No need to check for depth because the SeaMoth would already 
+            // be dead if you don't have the depth modules.
+            tempDamage.baseDamagePerSecond = 0.2f;
+            tempDamage.onlyLavaDamage = true;
+            tempDamage.minDamageTemperature = 50;
         }
     }
 
@@ -53,17 +68,21 @@ namespace MoreSeamothUpgrades.Patches
     [HarmonyPatch("Update")]
     public class Seamoth_Update_Patch
     {
+        // Reflection Method: AddEnergy
         static MethodInfo AddEnergyMethod = 
             typeof(Vehicle).GetMethod("AddEnergy", BindingFlags.NonPublic | BindingFlags.Instance);
 
         static void Prefix(SeaMoth __instance)
         {
+            // If we have the SeamothThermalModule equipped.
             var count = __instance.modules.GetCount(Main.SeamothThermalModule);
             if (count > 0)
             {
+                // Evaluate the energy to add based on temperature
                 var temperature = __instance.GetTemperature();
                 var energyToAdd = Main.ExosuitThermalReactorCharge.Evaluate(temperature);
 
+                // Add the energy by invoking private method using Reflection.
                 AddEnergyMethod.Invoke(__instance, new object[] { energyToAdd * UnityEngine.Time.deltaTime });
             }
         }
@@ -75,6 +94,7 @@ namespace MoreSeamothUpgrades.Patches
     { 
         static void Postfix(SeaMoth __instance, TechType techType)
         {
+            // Dictionary of TechTypes and their depth additions.
             var dictionary = new Dictionary<TechType, float>
             {
                 {
@@ -103,22 +123,32 @@ namespace MoreSeamothUpgrades.Patches
                 }
             };
 
+            // Depth upgrade to add.
             var depthUpgrade = 0f;
+
+            // Loop through all slots.
             for (int i = 0; i < SeamothUtility.slotIDs.Length; i++)
             {
+                // Get the slot and the TechType
                 var slot = SeamothUtility.slotIDs[i];
                 var techTypeInSlot = __instance.modules.GetTechTypeInSlot(slot);
 
+                // If its one of the depth modules
                 if (dictionary.ContainsKey(techTypeInSlot))
                 {
+                    // Get the depth upgrade for that TechType
                     var currentIteration = dictionary[techTypeInSlot];
+
+                    // If the upgrade is more than the one currently selected
                     if (currentIteration > depthUpgrade)
                     {
+                        // Select this one!
                         depthUpgrade = currentIteration;
                     }
                 }
             }
 
+            // Add the upgrade.
             __instance.crushDamage.SetExtraCrushDepth(depthUpgrade);
         }
     }
