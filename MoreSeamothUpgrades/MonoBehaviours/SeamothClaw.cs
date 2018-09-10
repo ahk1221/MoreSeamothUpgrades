9cbd338f9bcd23425a64c02976ac260191f7d8c3
Joseph Crowell <joseph.w.crowell@gmail.com>
2018-09-10 23:15:05 +1000

    Adding SeamothClaw.cs

---
+++ MoreSeamothUpgrades/MonoBehaviours/SeamothClaw.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using MoreSeamothUpgrades.Patches;
+
+namespace MoreSeamothUpgrades.MonoBehaviours
+{
+    public class SeamothClaw : MonoBehaviour
+    {
+        private SeaMoth seamoth;
+
+        void Start()
+        {
+            // Get Seamoth component on the current GameObject.
+            seamoth = GetComponent<SeaMoth>();
+        }
+    }
+}
